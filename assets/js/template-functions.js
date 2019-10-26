661171757259a49fb2d92e358a81c84ae949aa3a
Niag <niag@MacBook-Air-de-Niag.local>
2019-10-26 10:49:29 -0300

    - add sass variables, animations, start of menu stylization.

--- assets/js/template-functions.js
+++ assets/js/template-functions.js
@@ -1,6 +1,14 @@
-$(document).ready(function (){
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
+$('#menu-toggle').click(function(e) {
+    e.preventDefault();
+    $('#wrapper').toggleClass('toggled');
+});
+
+$('#menu-toggle').click(function() {
+    if ($('#open-close').hasClass("fa-long-arrow-left")) {
+        $('#open-close').removeClass('fa-long-arrow-left');
+        $('#open-close').addClass('fa-long-arrow-right');
+    } else {
+        $('#open-close').removeClass('fa-long-arrow-right');
+        $('#open-close').addClass('fa-long-arrow-left');
+    }
 });
